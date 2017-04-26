e28cf9881e87217a27c452d7b52b13f3dffbe669
Rob Ferguson <rob.ferguson@robferguson.org>
2017-04-26 12:21:41 +1000

    Updated the readme.md file

--- author/config/export.config.js
+++ author/config/export.config.js
@@ -2,16 +2,8 @@
 // provide a name for an entry, it can be anything such as 'copyAssets' or 'copyFonts'
 // then provide an object with a `src` array of globs and a `dest` string
 module.exports = {
-  copyAuthorAssets: {
-    src: ['{{WWW}}/assets/**/*'],
-    dest: '{{ROOT}}/../ui.content/src/main/resources/jcr_root/etc/author/assets'
-  },
-  copyAuthorBuild: {
-	src: ['{{WWW}}/build/**/*'],
-	dest: '{{ROOT}}/../ui.content/src/main/resources/jcr_root/etc/author/build'
-  },
   copyAuthorManifest: {
-	src: ['{{WWW}}/manifest.json', '{{WWW}}/service-worker.js'],
-    dest: '{{ROOT}}/../ui.content/src/main/resources/jcr_root/etc/author'
+	src: ['{{SRC}}/manifest.json', '{{SRC}}/service-worker.js'],
+	dest: '{{ROOT}}/../ui.content/src/main/resources/jcr_root/etc/author'
   }
 }
